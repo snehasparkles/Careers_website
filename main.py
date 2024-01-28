41d64254124f59457392273897774a0c786a5b00
snehasparkles <145270959+snehasparkles@users.noreply.github.com>
2024-01-28 07:58:10 +0000

    styles added

--- main.py
+++ main.py
@@ -2,10 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+job = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 12,00,000'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$120,000'
+  }
+]
 
 @app.route('/')
 def home():
-  return render_template("index.html")
+  return render_template("index.html",job = job)
 
 
 if __name__ == "__main__":
